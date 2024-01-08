0d9b208b0e24fee0f7e29ea020c9979254dea771
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-01-08 15:20:23 -0800

    systemslab v0.0.61 (new formula)
    Closes #1.
    
    Signed-off-by: Sean Lynch <42618346+swlynch99@users.noreply.github.com>

---
+++ Formula/systemslab.rb
@@ -0,0 +1,47 @@
+# Brew formula for the SystemsLab CLI.
+
+class Systemslab < Formula
+  desc      "CLI for interacting with SystemsLab servers"
+  homepage  "https://iop.systems"
+  url       "https://github.com/iopsystems/systemslab.git",
+    tag:    "v0.0.61"
+
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+
+  depends_on "bzip2"
+  depends_on "openssl"
+  depends_on "xz"
+
+  def install
+    system "cargo", "install",
+      "--bin", "systemslab",
+      "--path", "crates/cli",
+      "--profile", "prod",
+      "--locked",
+      "--root", prefix
+
+    pkgshare.install "crates/cli/jsonnet/systemslab.libsonnet"
+  end
+
+  test do
+    content = <<-EOM
+      local systemslab = import "systemslab.libsonnet";
+
+      {
+        name: 'test experiment',
+        jobs: {
+          test_job: {
+            steps: [
+              systemslab.bash('true')
+            ]
+          }
+        }
+      }
+    EOM
+
+    File.write("test.jsonnet", content)
+
+    system "#{bin}/systemslab", "evaluate", "test.jsonnet"
+  end
+end

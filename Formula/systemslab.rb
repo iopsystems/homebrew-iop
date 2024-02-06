d0619bbb5ae703fa84879050e09ad8279de0363d
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-02-06 07:23:49 +0000

    systemslab: update 0.0.67 bottle.

--- Formula/systemslab.rb
+++ Formula/systemslab.rb
@@ -9,10 +9,10 @@ class Systemslab < Formula
   license   :cannot_represent
 
   bottle do
-    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-0.0.65"
-    sha256 cellar: :any,                 arm64_ventura: "b9c4038ca50f36d734b8077276e677aa66456fc0e0b6cb593d3682fa44b2e6e9"
-    sha256 cellar: :any,                 ventura:       "7e2e2aaf4532738d3b0cfa1790208f26554fafeee79141a4dc31df474ad6f6ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2005771d99abc807efdfb59daab010b8046582dda57ae74c26deaa93ed23ab99"
+    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-0.0.67"
+    sha256 cellar: :any,                 arm64_ventura: "7ad429ce4de41d7e761682cfa9fd4b95ab35da18cb76d89f6d8babf566c13cb1"
+    sha256 cellar: :any,                 ventura:       "09d3a9f1f2bc78b5955ba96b16d890cb2dc6e0c44d420e8eb38659564851fe58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "569f6fe432d0731c6aac87f6134057dea2f5e1dfb1b08755cb0a3b800789a7ca"
   end
 
   depends_on "pkg-config" => :build

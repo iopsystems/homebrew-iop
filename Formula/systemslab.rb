8082572bed9a16b17b2b82de8f7e0f8982dce3e2
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2024-10-23 08:18:28 +0000

    systemslab: update 0.0.107 bottle.

--- Formula/systemslab.rb
+++ Formula/systemslab.rb
@@ -16,11 +16,11 @@ class Systemslab < Formula
   license   :cannot_represent
 
   bottle do
-    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-0.0.106"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e24c18fea90e2b3e135386bed2dd9ccd68349f65e7785405d181c26850b0833a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2174a8e5c727f4a4e6b74a5603132c6860932da763bc1d0dc5d7dcf3b66ec344"
-    sha256 cellar: :any_skip_relocation, ventura:       "1000b0f681f7ca1eca1b11d4440ce3a72b923fc23a465d4948093468abff5acf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b4b9c62d54e8582caf9b5b7f2b98648e81420ac0c0de9f86a1c6e15489704c2"
+    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-0.0.107"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "457316125372ad81307082a875c2447faa64373050138204dbac9077b08c134d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "531f5e1e0566ef4027f9d699c6edcfd4aa28efa0ff7bb9d99715747b503f4355"
+    sha256 cellar: :any_skip_relocation, ventura:       "2c61d8ff9840d36d7808d6e0bf1fdef1ffb94bc367c4729d5049691c73122e2b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c0b9d31204a185b9f69c53b4f3e32b6259beead819341cc6bae25da6275940f"
   end
 
   depends_on "go" => :build

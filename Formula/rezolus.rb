5b53a1495fec8fb15e379b6d8b29a863f2ae545b
Yao Yue <thinkingfish@gmail.com>
2025-06-26 14:38:35 -0700

    lint

--- Formula/rezolus.rb
+++ Formula/rezolus.rb
@@ -1,16 +1,15 @@
 # Brew formula for the SystemsLab CLI.
 
 class Rezolus < Formula
-  desc      "High-resolution, low-overhead systems telemetry via eBPF. Package contains metrics agent, multiple OTel or Parquet recorders, and an HTTP dashboard viewer."
+  desc      "High-resolution systems telemetry, including multiple recorders and a viewer."
   homepage  "https://rezolus.com"
   url       "https://github.com/iopsystems/rezolus.git",
     tag:      "v5.1.0",
     revision: "946dbc675733bfd15050ca2d7dfa443033cbe912"
   license any_of: ["Apache-2.0", "MIT"]
 
-  depends_on "rust" => :build
   depends_on "protobuf" => :build
-
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args

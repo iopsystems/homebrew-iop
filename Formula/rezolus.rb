25214c9894b569ece896aa3b2bdf435088387c55
Yao Yue <thinkingfish@gmail.com>
2025-06-26 16:30:50 -0700

    remove a dependency for runtime

--- Formula/rezolus.rb
+++ Formula/rezolus.rb
@@ -11,7 +11,6 @@ class Rezolus < Formula
   depends_on "llvm" => :build
   depends_on "protobuf" => :build
   depends_on "rust" => :build
-  depends_on "libelf"
   depends_on "zlib"
 
   def install

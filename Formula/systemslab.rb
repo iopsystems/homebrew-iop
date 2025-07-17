7639983728e090d1d1b74bc33ba526eda9d14d28
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2025-07-17 00:40:41 +0000

    systemslab: update 135.0.0 bottle.

--- Formula/systemslab.rb
+++ Formula/systemslab.rb
@@ -16,12 +16,12 @@ class Systemslab < Formula
   license   :cannot_represent
 
   bottle do
-    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-134.0.0"
-    sha256 cellar: :any,                 arm64_sequoia: "78800ff87dd7854c9a289fd5b42593e5601b29a4473fe3d6ebaf0d5960f494df"
-    sha256 cellar: :any,                 arm64_sonoma:  "9b1d8400927a6000a98b4f3396301d5ed9f6bf7204c5e9e2b4fe5ecd959c877f"
-    sha256 cellar: :any,                 arm64_ventura: "4c42c592c02f532fbab380a7e5c5347134aeb95b1c45d14b16123972e9032a64"
-    sha256 cellar: :any,                 ventura:       "fa300f4422051ca0ac20dd3bfaa1d28184907167eea092086e93e8c8ab4ee5c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9c2045d52ab6e8b7a0790ad1a8fe068ff77d6846acf493e76c2594c7e5f058e"
+    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/systemslab-135.0.0"
+    sha256 cellar: :any,                 arm64_sequoia: "0da62ab1317bdca4d3df791913af1e019358166e4959ca52309dc049c1c1d1f4"
+    sha256 cellar: :any,                 arm64_sonoma:  "cb8cb2c9afd29b33cca7f4865371d2a631fcb1f9634251c0d680867343aa76fc"
+    sha256 cellar: :any,                 arm64_ventura: "255e9e10ba55e7d1b65c7e6dc7bf6ae323cdee5edf6f79505ecdc81d4b1ef06d"
+    sha256 cellar: :any,                 ventura:       "53f2f57d3d7a518869f16aff608189fe8358fe192735fbcd9af5a1af55eb49a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b32f4aadf8763900acf7989dd65a0f37d5f6f50a233ffd6738f2ed48db17a20"
   end
 
   depends_on "go" => :build

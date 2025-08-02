376489449e51520b50345dbc290a78ca235e3db8
Yao Yue <thinkingfish@gmail.com>
2025-08-01 17:44:42 -0700

    Update rezolus.rb

--- Formula/rezolus.rb
+++ Formula/rezolus.rb
@@ -11,6 +11,7 @@ class Rezolus < Formula
   depends_on "llvm" => :build
   depends_on "protobuf" => :build
   depends_on "rust" => :build
+  depends_on "libelf"
   depends_on "zlib"
 
   def install

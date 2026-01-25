408db91bc6a0ea3c058c6fa8a6e49b8c196fe6a6
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2026-01-25 06:28:37 +0000

    rezolus: add 5.5.0 bottle.

--- Formula/rezolus.rb
+++ Formula/rezolus.rb
@@ -7,6 +7,12 @@ class Rezolus < Formula
   sha256 "12a23235223b8d967f1a987e3c7ace3a8af2e2fcaaebc97c16cee6931aa2849e"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    root_url "https://github.com/iopsystems/homebrew-iop/releases/download/rezolus-5.5.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c02e8bfe6f34908a0d9bb56a007d5117a1b23951e352b4bfe091b5dbb3b8afbf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44286542a5f76dff7d4059c22baa33a4bc3ac31b5e285fb96c8ebbdca46b9d6d"
+  end
+
   depends_on "llvm" => :build
   depends_on "protobuf" => :build
   depends_on "rust" => :build
